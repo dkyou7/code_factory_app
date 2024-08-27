f688008336f241a55fb8dd4d4cb058b4a178c443
youdongkwan <dkyou7@naver.com>
2024-08-27 20:42:26 +0900

    [feat] AppBar 와 TapBar 만들기

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,12 +1,46 @@
+import 'package:code_factory_app/common/const/colors.dart';
+import 'package:code_factory_app/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({Key? key}) : super(key: key);
 
   @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int index = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
+    return DefaultLayout(
+      title: '딜리버리',
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: INPUT_BG_COLOR,
+          selectedItemColor: PRIMARY_COLOR,
+          unselectedItemColor: BODY_TEXT_COLOR,
+          selectedFontSize: 10,
+          unselectedFontSize: 10,
+          type: BottomNavigationBarType.fixed,
+          onTap: (int index) {
+            setState(() {
+              print(index);
+              this.index = index;
+            });
+          },
+          currentIndex: index,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined), label: '홈'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.fastfood_outlined), label: '음식'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.receipt_long_outlined), label: '주문'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_outlined), label: '프로필'),
+          ]),
+      child: Center(
         child: Text('Root Tab'),
       ),
     );
