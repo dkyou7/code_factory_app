f3c17f8b21733297d9dcef854634c1b186eff72a
youdongkwan <dkyou7@naver.com>
2024-08-28 16:10:31 +0900

    [feat] fromJSON Mapping 3차

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:code_factory_app/common/const/data.dart';
 import 'package:code_factory_app/restaurant/component/RestaurantCard.dart';
 import 'package:code_factory_app/restaurant/model/restaurant_model.dart';
@@ -46,17 +48,8 @@ class RestaurantScreen extends StatelessWidget {
                       json: item,
                     );
 
-                    return RestaurantCard(
-                      image: Image.network(
-                        pItem.thumbUrl,
-                        fit: BoxFit.cover,
-                      ),
-                      name: pItem.name,
-                      tags: pItem.tags,
-                      ratingsCount: pItem.ratingsCount,
-                      deliveryTime: pItem.deliveryTime,
-                      deliveryFee: pItem.deliveryFee,
-                      ratings: pItem.ratings,
+                    return RestaurantCard.fromModel(
+                      model: pItem,
                     );
                   },
                   separatorBuilder: (_, index) {
