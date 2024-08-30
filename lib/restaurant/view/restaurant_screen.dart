b96b86fdb64c8d34422bfdc23f9b8f33695c137b
youdongkwan <dkyou7@naver.com>
2024-08-30 17:29:00 +0900

    [feat] Pagination UI 작업하기

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -4,6 +4,7 @@ import 'package:code_factory_app/common/const/data.dart';
 import 'package:code_factory_app/common/dio/dio.dart';
 import 'package:code_factory_app/restaurant/component/RestaurantCard.dart';
 import 'package:code_factory_app/restaurant/model/restaurant_model.dart';
+import 'package:code_factory_app/restaurant/repository/restaurant_repository.dart';
 import 'package:code_factory_app/restaurant/view/restaurant_detail_screen.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +12,7 @@ import 'package:flutter/material.dart';
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({Key? key}) : super(key: key);
 
-  Future<List> paginateRestaurant() async {
+  Future<List<RestaurantModel>> paginateRestaurant() async {
     final dio = Dio();
 
     dio.interceptors.add(
@@ -20,18 +21,22 @@ class RestaurantScreen extends StatelessWidget {
       ),
     );
 
-    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+    // final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+    //
+    // final resp = await dio.get(
+    //   'http://$ip/restaurant',
+    //   options: Options(
+    //     headers: {
+    //       'authorization': 'Bearer $accessToken',
+    //     },
+    //   ),
+    // );
 
-    final resp = await dio.get(
-      'http://$ip/restaurant',
-      options: Options(
-        headers: {
-          'authorization': 'Bearer $accessToken',
-        },
-      ),
-    );
+    final resp =
+    await RestaurantRepository(dio,baseUrl: 'http://$ip/restaurant')
+    .paginate();
 
-    return resp.data['data'];
+    return resp.data;
   }
 
   @override
@@ -40,9 +45,9 @@ class RestaurantScreen extends StatelessWidget {
       child: Center(
         child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: FutureBuilder<List>(
+            child: FutureBuilder<List<RestaurantModel>>(
               future: paginateRestaurant(),
-              builder: (context, AsyncSnapshot<List> snapshot) {
+              builder: (context, AsyncSnapshot<List<RestaurantModel>> snapshot) {
                 if (!snapshot.hasData) {
                   return Center(child: CircularProgressIndicator());
                 }
@@ -50,9 +55,9 @@ class RestaurantScreen extends StatelessWidget {
                 return ListView.separated(
                   itemCount: snapshot.data!.length,
                   itemBuilder: (_, index) {
-                    final item = snapshot.data![index];
+                    final pItem = snapshot.data![index];
                     // parsed
-                    final pItem = RestaurantModel.fromJson(item,);
+                    // final pItem = RestaurantModel.fromJson(item,);
 
                     return GestureDetector(
                         onTap: () {
