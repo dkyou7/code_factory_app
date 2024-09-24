9662d8b01451df2ff11bcb7c4c04a39d2ec60d1c
youdongkwan <dkyou7@naver.com>
2024-09-24 15:56:52 +0900

    [feat] RestaurantProvider 작업하기

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,10 +1,7 @@
-import 'dart:math';
-
-import 'package:code_factory_app/common/const/data.dart';
-import 'package:code_factory_app/common/dio/dio.dart';
 import 'package:code_factory_app/common/model/cursor_pagination_model.dart';
 import 'package:code_factory_app/restaurant/component/RestaurantCard.dart';
 import 'package:code_factory_app/restaurant/model/restaurant_model.dart';
+import 'package:code_factory_app/restaurant/provider/restaurant_provider.dart';
 import 'package:code_factory_app/restaurant/repository/restaurant_repository.dart';
 import 'package:code_factory_app/restaurant/view/restaurant_detail_screen.dart';
 import 'package:flutter/material.dart';
@@ -15,45 +12,38 @@ class RestaurantScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      child: Center(
-        child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: FutureBuilder<CursorPagination<RestaurantModel>>(
-              future: ref.watch(restaurantRepositoryProvider).paginate(),
-              builder: (context, AsyncSnapshot<CursorPagination<RestaurantModel>> snapshot) {
-                if (!snapshot.hasData) {
-                  return Center(child: CircularProgressIndicator());
-                }
+    final data = ref.watch(restaurantProvider);
 
-                return ListView.separated(
-                  itemCount: snapshot.data!.data.length,
-                  itemBuilder: (_, index) {
-                    final pItem = snapshot.data!.data[index];
-                    // parsed
-                    // final pItem = RestaurantModel.fromJson(item,);
+    if (data.length == 0) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
 
-                    return GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (_) => RestaurantDetailScreen(
-                                id: pItem.id,
-                              ),
-                            ),
-                          );
-                        },
-                        child: RestaurantCard.fromModel(
-                          model: pItem,
-                        ));
-                  },
-                  separatorBuilder: (_, index) {
-                    return SizedBox(height: 24.0);
-                  },
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: ListView.separated(
+        itemCount: data.length,
+        itemBuilder: (_, index) {
+          final pItem = data[index];
+          return GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => RestaurantDetailScreen(
+                      id: pItem.id,
+                    ),
+                  ),
                 );
               },
-            )),
-      ),
+              child: RestaurantCard.fromModel(
+                model: pItem,
+              ));
+        },
+        separatorBuilder: (_, index) {
+          return SizedBox(height: 24.0);
+        },
+      )
     );
   }
 }
