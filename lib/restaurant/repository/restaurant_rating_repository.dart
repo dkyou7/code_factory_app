4273d11f7434546d7ac47133917f15af75fd37af
youdongkwan <dkyou7@naver.com>
2024-10-09 16:46:32 +0900

    [feat] RestaurantRatingProvider 초기작업

--- lib/restaurant/repository/restaurant_rating_repository.dart
+++ lib/restaurant/repository/restaurant_rating_repository.dart
@@ -1,10 +1,21 @@
+import 'package:code_factory_app/common/const/data.dart';
 import 'package:code_factory_app/common/model/cursor_pagination_model.dart';
 import 'package:code_factory_app/common/model/pagination_params.dart';
 import 'package:code_factory_app/rating/model/rating_model.dart';
 import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/retrofit.dart';
 
+import '../../common/dio/dio.dart';
+
 part 'restaurant_rating_repository.g.dart';
+
+final restaurantRepositoryProvider
+= Provider.family<RestaurantRatingRepository,String>
+  ((ref,id) {
+    final dio = ref.watch(dioProvider);
+    return RestaurantRatingRepository(dio, baseUrl: 'http://$ip/restaurant/$id/rating');
+  });
 
 // http://$ip/restaurant/:rid/rating
 @RestApi()
