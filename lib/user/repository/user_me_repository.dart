740536ef52945e6be2867739eb2f5ef4d0b9e82c
youdongkwan <dkyou7@naver.com>
2024-10-16 21:51:55 +0900

    [feat] UserMeRepository 생성하기

---
+++ lib/user/repository/user_me_repository.dart
@@ -0,0 +1,30 @@
+import 'package:code_factory_app/common/const/data.dart';
+import 'package:code_factory_app/common/dio/dio.dart';
+import 'package:code_factory_app/user/model/user_model.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'user_me_repository.g.dart';
+
+final UserMeRepositoryProvider = Provider<UserMeRepository>(
+  (ref) {
+    final dio = ref.watch(dioProvider);
+
+    return UserMeRepository(
+      dio,
+      baseUrl: 'http://$ip/user/me',
+    );
+  },
+);
+
+@RestApi()
+abstract class UserMeRepository {
+  factory UserMeRepository(Dio dio, {String baseUrl}) = _UserMeRepository;
+
+  @GET('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<UserModel> getMe();
+}
