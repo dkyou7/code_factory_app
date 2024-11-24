cad7e5859b2b8fa754dc1af372c6c5ea6672dac8
youdongkwan <dkyou7@naver.com>
2024-11-24 22:26:14 +0900

    [feat] RefreshIndicator 작업하기

--- lib/common/component/pagination_list_view.dart
+++ lib/common/component/pagination_list_view.dart
@@ -94,33 +94,41 @@ class _PaginationListViewState<T extends IModelWithId>
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: ListView.separated(
-        controller: controller,
-        itemCount: cp.data.length + 1,
-        itemBuilder: (_, index) {
-          if (index == cp.data.length) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16.0,
-                vertical: 8.0,
-              ),
-              child: Center(
-                child: cp is CursorPaginationRefetchingMore
-                    ? const CircularProgressIndicator()
-                    : const Text('마지막 데이터입니다 ㅠㅠ'),
-              ),
-            );
-          }
-          final pItem = cp.data[index];
-          return widget.itemBuilder(
-            context,
-            index,
-            pItem
+      child: RefreshIndicator(
+        onRefresh: () async {
+          ref.read(widget.provider.notifier).paginate(
+            forceRefetch: true,
           );
         },
-        separatorBuilder: (_, index) {
-          return SizedBox(height: 24.0);
-        },
+        child: ListView.separated(
+          physics: AlwaysScrollableScrollPhysics(),
+          controller: controller,
+          itemCount: cp.data.length + 1,
+          itemBuilder: (_, index) {
+            if (index == cp.data.length) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 8.0,
+                ),
+                child: Center(
+                  child: cp is CursorPaginationRefetchingMore
+                      ? const CircularProgressIndicator()
+                      : const Text('마지막 데이터입니다 ㅠㅠ'),
+                ),
+              );
+            }
+            final pItem = cp.data[index];
+            return widget.itemBuilder(
+              context,
+              index,
+              pItem
+            );
+          },
+          separatorBuilder: (_, index) {
+            return SizedBox(height: 24.0);
+          },
+        ),
       ),
     );
   }
